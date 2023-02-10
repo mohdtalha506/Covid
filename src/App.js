3212addb8c2a5c51a5b58515791e150267383640
mohdtalha506 <mohdtalha506@gmail.com>
2023-02-10 09:31:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import StatewiseData from './components/StatewiseData'
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Covid19 from './components/Countries'
 import Navbar from './components/Navbar'
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Home from './components/Home'
 import Search from './components/Search'
 import Region from './components/Region'
 import Countries from './components/Countries'
-
 
 
 const App = () => {
